56a8a05c3bf4c6a8451d242add807240da243dab
Salim Mayan <mailsalim@gmail.com>
2021-05-24 20:49:37 -0700

    completed project

---
+++ ToDoList/Models/Category.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+  public class Category
+  {
+    public Category()
+    {
+      this.JoinEntities = new HashSet<CategoryItem>();
+    }
+
+    public int CategoryId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<CategoryItem> JoinEntities { get; set; }
+
+    //  public virtual ICollection<CategoryItem> JoinEntities { get;}
+  }
+}
